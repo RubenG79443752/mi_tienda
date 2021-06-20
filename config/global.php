6ca2a42c4f33333047d8c2932102ccca571d5032
RubenG79443752 <fab79443752@gmail.com>
2021-06-20 14:02:13 -0400

    asd

--- config/global.php
+++ config/global.php
@@ -1,16 +1,16 @@
 <?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
+define("DB_HOST", "z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
 
 // nombre de la base de datos
-define("DB_NAME", "dbsistema");
+define("DB_NAME", "o2a74u49aegrcmc3");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "nw2dpetrzcp7hwd4");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "vbb5iuwcrv6vepr6");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
